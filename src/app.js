5626527d9dfc0102fae4a8c805ccfc78d102c892
Stephen Ramsay <rabrandom@gmail.com>
2020-01-14 18:49:07 +0000

    made a function to multiply 2 numbers. doesnt work

--- src/app.js
+++ src/app.js
@@ -4,10 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
-    },
+      currencies: {
+        name: "",
+        rate: null
+      },
+      amountInput: null,
+      selectedCurrency: null
+      },
+      mounted(){
+        this.currencyFetch()
+      },
     methods: {
-
-    }
+      currencyFetch: function(){
+        fetch('https://api.exchangeratesapi.io/latest')
+        .then(req => req.json())
+        .then(data => this.currencies = data.rates)
+      },
+      // write a method that multiplies
+      multiplyAmounts: function(){
+      this.amountInput *= selectedCurrency
+    )}
   });
 });
